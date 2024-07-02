64f93458159ffe6d9df2c3f6d9801e327e784973
Nolan Welch <nolanwelch@outlook.com>
2024-07-02 23:20:04 +0200

    Finish implementing client.py

--- bookeo/client.py
+++ bookeo/client.py
@@ -1,14 +1,17 @@
-import json
-
 import requests
 
-from .availability import Availability
+from .availability import BookeoAvailability
+from .bookings import BookeoBookings
+from .customers import BookeoCustomers
+from .holds import BookeoHolds
+from .payments import BookeoPayments
 from .request import BookeoRequest
-
-# https://www.bookeo.com/apiref
-
-
-# TODO: Figure out some standard way of passing datetimes back to the user.
+from .resourceblocks import BookeoResourceBlocks
+from .seatblocks import BookeoSeatblocks
+from .settings import BookeoSettings
+from .setup import VERSION
+from .subaccounts import BookeoSubaccounts
+from .webhooks import BookeoWebhooks
 
 
 class BookeoClientException(Exception):
@@ -20,16 +23,41 @@ class BookeoClientException(Exception):
 
 
 class BookeoClient:
-    BASE_URL = "https://api.bookeo.com/v2"
-
     def __init__(self, secret_key: str, api_key: str):
-        if secret_key is None:
-            raise ValueError("secret_key cannot be None")
-        if api_key is None:
-            raise ValueError("api_key cannot be None")
-        self.secret_key = secret_key
-        self.api_key = api_key
+        if secret_key is None or api_key is None:
+            raise BookeoClientException("Must initialize secret_key and api_key")
+        self._secret_key = secret_key
+        self._api_key = api_key
+        # API modules
+        self.availability = BookeoAvailability(self)
+        self.bookings = BookeoBookings(self)
+        self.customers = BookeoCustomers(self)
+        self.holds = BookeoHolds(self)
+        self.payments = BookeoPayments(self)
+        self.resourceblocks = BookeoResourceBlocks(self)
+        self.seatblocks = BookeoSeatblocks(self)
+        self.settings = BookeoSettings(self)
+        self.subaccounts = BookeoSubaccounts(self)
+        self.webhooks = BookeoWebhooks(self)
 
-    def request(self, *args, **kwargs):
+    def query_dict(self) -> dict:
+        """Returns the base query dictionary for Bookeo API requests."""
+        return {"secretKey": self._secret_key, "apiKey": self._api_key}
+
+    def base_url(self) -> str:
+        """Returns the base URL for Bookeo API requests."""
+        return "https://api.bookeo.com/v2"
+
+    def headers(self) -> dict:
+        """Returns the standard headers for Bookeo API requests."""
+        return {
+            "Cache-Control": "no-cache",
+            "User-Agent": f"PythonBookeo/{VERSION}",
+            "Accept": "text/html,application/json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+        }
+
+    def request(self, *args, **kwargs) -> requests.Response:
         r = BookeoRequest(self, *args, **kwargs)
         return r.request()
