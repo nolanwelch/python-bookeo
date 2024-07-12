e63db25f43328930be83e3f0ee09329c6f151e71
Nolan Welch <nolanwelch@outlook.com>
2024-07-12 15:40:13 -0400

    Configure repository for 0.1.0 release

--- tests/context.py
+++ tests/context.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import bookeo
+import src
 
 
 # https://docs.python-guide.org/writing/structure/ (see "test suite")
