c0686b61ebd4c2efe3a415eeca5c96175b5e8434
Nolan Welch <nolanwelch@outlook.com>
2024-07-12 20:13:53 -0400

    Consolidate request functionality

--- bookeo/client.py
+++ bookeo/client.py
@@ -1,9 +1,67 @@
 import json
+from datetime import datetime
 from typing import Optional, Union
 from urllib.parse import urljoin
 
+import pytz
 import requests
 
+VERSION = "0.1.0"
+BOOKEO_TS_FORMAT = r"%Y-%m-%dT%H:%M:%SZ"
+
+
+class BookeoClientException(Exception):
+    """Class for exceptions related to the Bookeo client."""
+
+    pass
+
+
+class BookeoClient:
+    def __init__(self, secret_key: str, api_key: str):
+        if secret_key is None or api_key is None:
+            raise TypeError("Must initialize secret_key and api_key")
+        self._secret_key = secret_key
+        self._api_key = api_key
+
+    def query_dict(self) -> dict:
+        """Returns the base query dictionary for Bookeo API requests."""
+        return {"secretKey": self._secret_key, "apiKey": self._api_key}
+
+    def base_url(self) -> str:
+        """Returns the base URL for Bookeo API requests."""
+        return "https://api.bookeo.com/v2"
+
+    def headers(self) -> dict:
+        """Returns the standard headers for Bookeo API requests."""
+        return {
+            "Cache-Control": "no-cache",
+            "User-Agent": f"PythonBookeo/{VERSION}",
+            "Accept": "text/html,application/json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+        }
+
+    def _request(self, *args, **kwargs) -> requests.Response:
+        r = BookeoRequest(self, *args, **kwargs)
+        return r.request()
+
+
+def bookeo_timestamp_to_dt(timestamp: Optional[str]) -> Optional[datetime]:
+    if timestamp is None:
+        return None
+
+    dt = datetime.strptime(timestamp, BOOKEO_TS_FORMAT)
+    return pytz.utc.localize(dt)
+
+
+def dt_to_bookeo_timestamp(dt: Optional[datetime]) -> Optional[str]:
+    if dt is None:
+        return None
+
+    if dt.tzinfo is not None:
+        # assume naive datetimes are in UTC
+        dt = dt.astimezone(pytz.timezone("UTC"))
+    return datetime.strftime(dt, BOOKEO_TS_FORMAT)
 
 
 class BookeoRequestException(Exception):
@@ -26,8 +84,11 @@ class BookeoRequest:
         self,
         client: BookeoClient,
         path: str,
+        params: dict = None,
+        data: Union[dict, str] = {},
         method: str = "GET",
     ):
+        self.params = params or {}
         self.params.update(client.query_dict())
         self.data = data
         if self.data is str:
@@ -63,6 +124,8 @@ class BookeoRequest:
 class BookeoRequestPager:
     """Pages the requested Bookeo API method and returns the JSON data of each response."""
 
+    def __init__(self, request: BookeoRequest, items_per_page: int = 50):
+        if "pageNavigationToken" in request.params.keys():
             raise BookeoRequestException("Paged URL cannot include navigation token")
         self._request = request
         self._items_per_page = items_per_page
